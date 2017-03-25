ae1fb955e6a8d22cc829d22b595300f8394c4444
low_yms <578479132@qq.com>
2017-03-26 01:46:13 +0800

    再次提交 尝试命令行

--- app/src/main/java/com/example/apen/yms_auto_update/MainActivity.java
+++ app/src/main/java/com/example/apen/yms_auto_update/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.apen.yms_auto_update;
 
-import android.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //稍微修改一下，不然不能提交
 
         initActionBar();
 
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void initActionBar() {
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(false);
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(false);
     }
 }
