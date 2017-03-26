effae5ea611d45753bae37c2a896afcc7facaee5
low_yms <578479132@qq.com>
2017-03-26 11:52:41 +0800

    闪屏页的实现

--- app/src/main/java/com/example/apen/yms_auto_update/activity/Splash_Activity.java
+++ app/src/main/java/com/example/apen/yms_auto_update/activity/Splash_Activity.java
@@ -1,15 +1,61 @@
 package com.example.apen.yms_auto_update.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
+import com.example.apen.yms_auto_update.MainActivity;
 import com.example.apen.yms_auto_update.R;
 
 public class Splash_Activity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_);
+
+        initView();
+    }
+
+
+    /**
+     * 初始化view
+     */
+    private void initView() {
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        doAction();
+    }
+
+    private void doAction() {
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+
+                try {
+                    Thread.sleep(2000);
+
+                    startAction();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+    }
+
+    private void startAction() {
+
+        startActivity(new Intent(Splash_Activity.this, MainActivity.class));
+        finish();
+
     }
 }
